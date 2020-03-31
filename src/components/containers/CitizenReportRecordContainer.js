4ee495f55d19de5cdcb922c9f439cf67c081c9f0
Hosmel Quintana <hosmelq@gmail.com>
2020-03-31 17:58:46 -0600

    rename components

---
+++ src/components/containers/CitizenReportRecordContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CitizenReportRecordForm from '../forms/CitizenReportRecordForm'
+
+const CitizenReportRecordContainer = () => {
+  return (
+    <div className="container">
+      <CitizenReportRecordForm />
+    </div>
+  )
+}
+
+export default CitizenReportRecordContainer
